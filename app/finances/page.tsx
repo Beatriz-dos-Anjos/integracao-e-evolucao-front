ae106d4d8926ea405ae67f3c207c1dff4de8de41
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-07-25 18:31:37 -0300

    hotfix: build

--- app/finances/page.tsx
+++ app/finances/page.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { DollarSign, Plus, Trash2, ArrowLeft, Home, X, Check } from "lucide-react"
+import { DollarSign, Plus, Trash2, ArrowLeft, X, Check } from "lucide-react"
 import api from "@/services/api"
 import { Edit } from "lucide-react"
 
